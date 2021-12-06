882d8cf129f7b7a3e259492997e7927670554bfc
Jason Xu <40355221+JasonBrave@users.noreply.github.com>
2021-12-06 10:36:06 -0500

    Implement randombot simulation, calculate optimal price without assumption

--- src/GPCGame2021/GPCGame2021.java
+++ src/GPCGame2021/GPCGame2021.java
@@ -1,16 +1,10 @@
 package GPCGame2021;
 
 import GPCGame2021.GameDetails;
+import java.util.Random;
 
 class GPCGame2021 {
 	private static GPCGame2021 instance = null;
-
-	private static float past_price = 0;
-	private static int past_customer = 0;
-	private static float past_revenue = 0;
-	private static float past_expense = 0;
-	private static float past_profit = 0;
-	private static float total = 0;
 
 	/**
 	 * Only submit the method below
@@ -27,13 +21,34 @@ class GPCGame2021 {
 		float sold_more_dec; // number more sold per 1$ decrease in price
 		cost_per_item = currentGameDetails.expenses.get(currentGameDetails.myIndex)
 						/ currentGameDetails.customers.get(currentGameDetails.myIndex);
-		sold_default = 30000;
-		sold_more_dec
-			= (currentGameDetails.customers.get(currentGameDetails.myIndex) - sold_default)
-			  / (100 - currentGameDetails.prices.get(currentGameDetails.myIndex));
+		int another_team = currentGameDetails.myIndex + 1;
+		if (another_team >= 6) {
+			another_team = 0;
+		}
+		sold_more_dec = (currentGameDetails.customers.get(currentGameDetails.myIndex)
+						 - currentGameDetails.customers.get(another_team))
+						/ (currentGameDetails.prices.get(another_team)
+						   - currentGameDetails.prices.get(currentGameDetails.myIndex));
+		sold_default
+			= currentGameDetails.customers.get(currentGameDetails.myIndex)
+			  - (100 - currentGameDetails.prices.get(currentGameDetails.myIndex)) * sold_more_dec;
 		float opt_price = (sold_default + 100 * sold_more_dec + sold_more_dec * cost_per_item)
-						  / (2 * sold_more_dec);
-		return opt_price;
+						  / (2 * sold_more_dec); // set derivative to zero and solve for x
+		// check endpoints
+		if (opt_price <= 0
+			|| opt_price >= 100) { // maxima of profit is out of domain, test endpoint
+			float profit_at_zero
+				= (0 - cost_per_item) * (sold_default + 100 * sold_more_dec - sold_default * 0);
+			float profit_at_hundred
+				= (100 - cost_per_item) * (sold_default + 100 * sold_more_dec - sold_default * 100);
+			if (profit_at_zero > profit_at_hundred) {
+				return (float)0.5;
+			} else {
+				return (float)99.5;
+			}
+		} else {
+			return opt_price;
+		}
 	}
 
 	public static void main(String[] args) {
@@ -47,29 +62,44 @@ class GPCGame2021 {
 			gamedetails.profit.add((float)0);
 			gamedetails.total.add((float)0);
 		}
-		final float cost_per_item = 25; // cost per sweater
-		final float sold_default = 30000; // number sold at default price
-		final float sold_more_dec = 1000; // number more sold per 1$ decrease in price
+		final float cost_per_item = 5; // cost per sweater
+		final float sold_default = 6532; // number sold at default price
+		final float sold_more_dec = 2345; // number more sold per 1$ decrease in price
+
+		Random rnd = new Random();
 		for (int i = 1; i <= 5; i++) {
 			gamedetails.round = i;
 			gamedetails.myIndex = 0;
-			gamedetails.prices.set(0, past_price);
-			gamedetails.customers.set(0, past_customer);
-			gamedetails.revenue.set(0, past_revenue);
-			gamedetails.expenses.set(0, past_expense);
-			gamedetails.profit.set(0, past_profit);
-			gamedetails.total.set(0, total);
 			float price = GPCGame2021.getInstance().strategy_6854(gamedetails);
 			System.out.printf("Round %d, sweater price %f dollar\n", i, price);
-			past_price = price;
-			past_customer = (int)(sold_default + sold_more_dec * (100 - price));
-			past_revenue = past_customer * price;
-			past_expense = past_customer * cost_per_item;
-			past_profit = past_revenue - past_expense;
-			total += past_profit;
+			gamedetails.prices.set(0, price);
+			gamedetails.customers.set(0, (int)(sold_default + sold_more_dec * (100 - price)));
+			gamedetails.revenue.set(0, gamedetails.customers.get(0) * price);
+			gamedetails.expenses.set(0, gamedetails.customers.get(0) * cost_per_item);
+			gamedetails.profit.set(0, gamedetails.revenue.get(0) - gamedetails.expenses.get(0));
+			gamedetails.total.set(0, gamedetails.total.get(0) + gamedetails.profit.get(0));
 			System.out.printf(
 				"Round %d, customer %d, renenue %f, expense %f, profit %f, total %f\n", i,
-				past_customer, past_revenue, past_expense, past_profit, total);
+				gamedetails.customers.get(0), gamedetails.revenue.get(0),
+				gamedetails.expenses.get(0), gamedetails.profit.get(0), gamedetails.total.get(0));
+			// randombots
+			for (int bot = 1; bot <= 5; bot++) {
+				price = rnd.nextFloat() * 100;
+				System.out.printf("Round %d, bot %d sweater price %f dollar\n", i, bot, price);
+				gamedetails.prices.set(bot, price);
+				gamedetails.customers.set(bot, (int)(sold_default + sold_more_dec * (100 - price)));
+				gamedetails.revenue.set(bot, gamedetails.customers.get(bot) * price);
+				gamedetails.expenses.set(bot, gamedetails.customers.get(bot) * cost_per_item);
+				gamedetails.profit.set(bot, gamedetails.revenue.get(bot)
+												- gamedetails.expenses.get(bot));
+				gamedetails.total.set(bot,
+									  gamedetails.total.get(bot) + gamedetails.profit.get(bot));
+				System.out.printf(
+					"Round %d bot %d, customer %d, renenue %f, expense %f, profit %f, total %f\n",
+					i, bot, gamedetails.customers.get(bot), gamedetails.revenue.get(bot),
+					gamedetails.expenses.get(bot), gamedetails.profit.get(bot),
+					gamedetails.total.get(bot));
+			}
 		}
 	}
 
